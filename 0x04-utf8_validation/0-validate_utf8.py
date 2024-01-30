28ce48f700f27e6ba1d4299aa743d80c74cba52b
Akuna Joshua <akunajoshua@gmail.com>
2024-01-30 07:23:57 +0100

    0-validate_utf8.py : code refactored

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
-"""defines the validUTF8 function
 """
-def validUTF8(data)
-    """
-    returns True if data is a valid UTF8 encoding else False
+Write a method that determines if a given data set
+represents a valid UTF-8 encoding.
+
+Prototype: def validUTF8(data)
+Return: True if data is a valid UTF-8 encoding, else
+return False
+A character in UTF-8 can be 1 to 4 bytes long
+The data set can contain multiple characters
+The data will be represented by a list of integers
+Each integer represents 1 byte of data, therefore you
+only need to handle the 8 least significant bits of each
+integer
+"""
+
+
+def validUTF8(data):
+    """_summary_
+
+    Args:
+            data (list[int]): a list of integers
     """
     expected_continuation_bytes = 0
 
