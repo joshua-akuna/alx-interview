129fe17981d94a2014b8c0ce1d58f66753ee31bd
Akuna Joshua <akunajoshua@gmail.com>
2024-03-07 07:28:48 +0100

    0-island_perimeter.py initial commit

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+The module defines the island_perimeter function
+'''
+
+
+def island_perimeter(grid):
+    '''
+    returns the perimeter of the island defined in grid
+    '''
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
